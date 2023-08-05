d71168aec4d8c20210143df6de4c5b7868ab94ce
nitesh-dev <niteshdev547@gmail.com>
2023-08-05 22:19:59 +0530

    sidebar updated

---
+++ pages/admin/index.vue
@@ -0,0 +1,43 @@
+<script setup lang='ts'>
+const tabs = Array<{ name: string, url: string }>()
+tabs.push({
+    name: 'Dashboard',
+    url: '/admin'
+}) 
+tabs.push({
+    name: 'Consultants',
+    url: '/admin/consultants'
+}) 
+tabs.push({
+    name: 'Leads',
+    url: '/admin/leads'
+}) 
+tabs.push({
+    name: 'Plans',
+    url: '/admin/plans'
+}) 
+tabs.push({
+    name: 'Commission',
+    url: '/admin/commission'
+}) 
+tabs.push({
+    name: 'Withdraw',
+    url: '/admin/withdraw'
+}) 
+tabs.push({
+    name: 'Account',
+    url: '/admin/account'
+}) 
+</script>
+<template>
+    <div class="panel">
+        <Sidebar :active-tab="0" :tab-data="tabs"></Sidebar>
+    </div>
+</template>
+<style scoped>
+.panel {
+    width: 100%;
+    min-height: 100vh;
+    background-color: var(--color-surface-variant);
+}
+</style>
