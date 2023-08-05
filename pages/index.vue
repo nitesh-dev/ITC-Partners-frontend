3fd206103e6a96e94f496c8cbb9b220ab07ffa88
nitesh-dev <niteshdev547@gmail.com>
2023-08-05 21:28:32 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <div class="panel">
+        <Sidebar></Sidebar>
+    </div>
+</template>
+<style scoped>
+.panel{
+    width: 100%;
+    min-height: 100vh;
+    background-color: var(--color-surface-variant);
+}
+
+</style>
