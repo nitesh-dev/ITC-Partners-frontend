7fad3be2e70c18427be882e284774b795326ae88
nitesh-dev <niteshdev547@gmail.com>
2023-08-06 07:57:12 +0530

    bugs fixed

---
+++ components/Profile.vue
@@ -0,0 +1,46 @@
+<script setup lang='ts'>
+defineProps<{
+    name: string,
+    role: string
+}>()
+</script>
+<template>
+    <div class="profile">
+        <img src="../public/images/no_image.png">
+        <div>
+            <h4>{{ name }}</h4>
+            <span>{{ role }}</span>
+        </div>
+    </div>
+</template>
+<style scoped>
+.profile{
+    border-radius: var(--normal-radius);
+    background-color: var(--color-surface);
+    min-width: 250px;
+    height: 68px;
+    width: max-content;
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    padding: 0 10px;
+}
+
+.profile img{
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+
+.profile h4{
+    margin: 0;
+    font-weight: normal;
+}
+.profile span{
+    font-size: var(--small-font);
+    color: var(--color-primary);
+    font-weight: bold;
+}
+
+</style>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -5,8 +5,145 @@ import { tabs } from '~/data/admin'
     <div class="panel">
         <Sidebar :active-tab="0" :tab-data="tabs"></Sidebar>
 
+        <!-- header -->
+        <div class="header">
+            <Profile name="Nitesh kr" role="Admin" />
+        </div>
+
+        <h2>Dashboard</h2>
+        <div class="reports">
+            <div class="card">
+                <p>Total Earning</p>
+                <span>₹100,500</span>
+            </div>
+            <div class="card">
+                <p>Total Withdraw</p>
+                <span>₹100,500</span>
+            </div>
+            <div class="card">
+                <p>Total Profits</p>
+                <span>₹100,500</span>
+            </div>
+            <div class="card">
+                <p>Total Consultants</p>
+                <span>49</span>
+            </div>
+        </div>
+
+
+        <!-- statistics -->
+        <h3>Statistics</h3>
+        <div class="statistics">
+            <!-- 1 -->
+            <div class="card">
+                <p class="title">Earning history</p>
+                <hr>
+                <div class="graph"></div>
+            </div>
+
+            <!-- 2 -->
+            <div class="card">
+                <p class="title">Withdraw history</p>
+                <hr>
+                <div class="graph"></div>
+            </div>
+
+            <!-- 3 -->
+            <div class="card">
+                <p class="title">Net profit history</p>
+                <hr>
+                <div class="graph"></div>
+            </div>
+
+            <!-- 4 -->
+            <div class="card">
+                <p class="title">Leads history</p>
+                <hr>
+                <div class="graph"></div>
+            </div>
+
+
+        </div>
+
     </div>
 </template>
 <style scoped>
+
+.card{
+    background-color: var(--color-surface);
+    border-radius: var(--normal-radius);
+}
+.panel {
+    overflow: auto;
+    padding-left: 3rem;
+    padding-right: 1rem;
+}
+
+.panel .header {
+    margin: 2rem 0;
+    display: flex;
+    justify-content: flex-end;
+}
+
+
+/* ------------------- reports -------------------*/
+.panel>.reports{
+    margin-top: 2rem;
+    margin-bottom: 3rem;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+
+}
+
+.panel>.reports .card{
+    height: 100px;
+    width: 260px;
+    padding: 0 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.panel>.reports p{
+    font-size: var(--small-font);
+    margin: 0;
+    margin-bottom: 0.5em;
+}
+
+.panel>.reports span{
+    font-size: var(--average-2-font);
+    color: var(--color-primary);
+}
+
+
+
+
+/* -------------------- statistics --------------- */
+
+.panel .statistics{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+}
+
+.panel .statistics .title{
+    margin: 1em;
+}
+
+.panel .statistics .card{
+    width: 500px;
+}
+
+.panel .statistics hr{
+    border: none;
+    border-bottom: 2px solid var(--color-surface-variant);
+}
+
+.panel .statistics .graph{
+    min-height: 300px;
+}
+
+
 
 </style>
