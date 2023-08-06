768d3dc715345c63ffcf6e533371152eaa8ca0d2
nitesh-dev <niteshdev547@gmail.com>
2023-08-06 20:56:57 +0530

    offer page completed

--- pages/admin/offers.vue
+++ pages/admin/offers.vue
@@ -1,11 +1,121 @@
 <script setup lang='ts'>
-import { tabs } from '~/data/admin' 
+import { Offer } from 'data/dataTypes';
+import { tabs } from '~/data/admin'
+import { dateTimeString } from '~/extra/utils'
+
+const offers = ref(Array<Offer>())
+offers.value.push({
+    id: '1',
+    category_id: '2',
+    name: 'sdfsadf',
+    price: 54545,
+    discount: 500,
+    description: 'jkjkdlfj sdfjklasdjfksa dfjksaldfkljasdfkl jsdfjasdf',
+    image: '',
+    start_datetime: 0,
+    end_datetime: 0,
+    label: 'Mega offer'
+})
+
+offers.value.push({
+    id: '1',
+    category_id: '2',
+    name: 'sdfsadf',
+    price: 54545,
+    discount: 500,
+    description: 'jkjkdlfj sdfjklasdjfksa dfjksaldfkljasdfkl jsdfjasdf',
+    image: '',
+    start_datetime: 0,
+    end_datetime: 0,
+    label: 'Mega offer'
+})
+
+offers.value.push({
+    id: '1',
+    category_id: '2',
+    name: 'sdfsadf',
+    price: 54545,
+    discount: 500,
+    description: 'jkjkdlfj sdfjklasdjfksa dfjksaldfkljasdfkl jsdfjasdf',
+    image: '',
+    start_datetime: 0,
+    end_datetime: 0,
+    label: 'Mega offer'
+})
+
+
+const activeTabIndex = ref(0)
+
+function onTabChange(index: number) {
+    // alert(index)
+    activeTabIndex.value = index
+}
+
 </script>
 <template>
     <div class="panel">
         <Sidebar :active-tab="4" :tab-data="tabs"></Sidebar>
+
+        <!-- header -->
+        <div class="header">
+            <Profile name="Nitesh kr" role="Admin" />
+        </div>
+
+        <h2>Offers</h2>
+        <button class="primary">Add Offer</button>
+        <WidgetsTab :active-tab="activeTabIndex" :names="['Active', 'Expired']" :onChange="event => onTabChange(event)">
+        </WidgetsTab>
+
+        <!-- Active -->
+        <div v-if="activeTabIndex == 0" class="tab-container">
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Discount</th>
+                        <th>Offer Start</th>
+                        <th>Offer End</th>
+                        <th>Label</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="spacer">
+                        <td></td>
+                    </tr>
+                    <template v-for="item, index in offers">
+
+                        <!-- TODO add condition to check offer expiry -->
+                        <tr>
+                            <td><img src="~/public/images/no_image.png"></td>
+                            <td>{{ item.name }}</td>
+                            <td>{{ item.price }}</td>
+                            <td>{{ item.discount }}</td>
+                            <td>{{ dateTimeString(item.start_datetime) }}</td>
+                            <td>{{ dateTimeString(item.end_datetime) }}</td>
+                            <td><span class="chip normal">{{ item.label }}</span></td>
+                            <td>
+                                <svg class="normal" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M13.94 5 19 10.06 9.062 20a2.25 2.25 0 0 1-.999.58l-5.116 1.395a.75.75 0 0 1-.92-.921l1.395-5.116a2.25 2.25 0 0 1 .58-.999L13.938 5Zm7.09-2.03a3.578 3.578 0 0 1 0 5.06l-.97.97L15 3.94l.97-.97a3.578 3.578 0 0 1 5.06 0Z" />
+                                </svg>
+                            </td>
+                            <td>
+                                <svg class="danger" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M21.5 6a1 1 0 0 1-.883.993L20.5 7h-.845l-1.231 12.52A2.75 2.75 0 0 1 15.687 22H8.313a2.75 2.75 0 0 1-2.737-2.48L4.345 7H3.5a1 1 0 0 1 0-2h5a3.5 3.5 0 1 1 7 0h5a1 1 0 0 1 1 1Zm-7.25 3.25a.75.75 0 0 0-.743.648L13.5 10v7l.007.102a.75.75 0 0 0 1.486 0L15 17v-7l-.007-.102a.75.75 0 0 0-.743-.648Zm-4.5 0a.75.75 0 0 0-.743.648L9 10v7l.007.102a.75.75 0 0 0 1.486 0L10.5 17v-7l-.007-.102a.75.75 0 0 0-.743-.648ZM12 3.5A1.5 1.5 0 0 0 10.5 5h3A1.5 1.5 0 0 0 12 3.5Z" />
+                                </svg>
+                            </td>
+                        </tr>
+                    </template>
+
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
