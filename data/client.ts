38816ca4d7a1b796c345b43d72d6019af775d806
nitesh-dev <niteshdev547@gmail.com>
2023-08-07 15:35:26 +0530

    client sidebar completed

---
+++ data/client.ts
@@ -0,0 +1,33 @@
+
+
+// client sidebar tabs  
+export const tabs = Array<{ name: string, url: string }>()
+tabs.push({
+    name: 'Dashboard',
+    url: '/client'
+}) 
+tabs.push({
+    name: 'Offers',
+    url: '/client/offers'
+}) 
+tabs.push({
+    name: 'Leads',
+    url: '/client/leads'
+}) 
+tabs.push({
+    name: 'Referrals',
+    url: '/client/referrals'
+}) 
+
+tabs.push({
+    name: 'Withdraw',
+    url: '/admin/withdraw'
+}) 
+tabs.push({
+    name: 'Account',
+    url: '/admin/account'
+})
+
+
+
+
---
+++ pages/client/index.vue
@@ -0,0 +1,102 @@
+<script setup lang='ts'>
+import { tabs } from '~/data/client' 
+import {earningHistoryRaw, withdrawHistoryRaw, profitHistoryRaw, leadsHistoryRaw} from '~/data/adminRawData'
+</script>
+<template>
+    <div class="panel">
+        <Sidebar :active-tab="0" :tab-data="tabs"></Sidebar>
+
+        <!-- header -->
+        <div class="header">
+            <Profile name="Nitesh kr" role="Admin" />
+        </div>
+
+        <h2>Dashboard</h2>
+        <div class="reports">
+            <div class="card">
+                <p>Total Earning</p>
+                <span>₹100,500</span>
+                <div class="progress secondary"></div>
+            </div>
+            <div class="card">
+                <p>Total Withdraw</p>
+                <span>₹100,500</span>
+                <div class="progress neutral"></div>
+            </div>
+            <div class="card">
+                <p>Total Profits</p>
+                <span>₹100,500</span>
+                <div class="progress success"></div>
+            </div>
+            <div class="card">
+                <p>Total Consultants</p>
+                <span>49</span>
+            </div>
+        </div>
+
+
+        <!-- statistics -->
+        <h3>Statistics</h3>
+        <div class="statistics">
+            <!-- 1 -->
+            <div class="card">
+                <p class="title">Earning history</p>
+                <hr>
+                <LineChart borderColor="#1665D8" backgroundColor="#EAEEF5" :myChartData="earningHistoryRaw"/>
+            </div>
+
+            <!-- 2 -->
+            <div class="card">
+                <p class="title">Withdraw history</p>
+                <hr>
+                <LineChart borderColor="#d33396" backgroundColor="#f5e6ef" :myChartData="withdrawHistoryRaw"/>
+            </div>
+
+            <!-- 3 -->
+            <div class="card">
+                <p class="title">Net profit history</p>
+                <hr>
+                <DoughnutChart  :backgroundColors="['#1665D8','#d33396', '#F6AB2F']" :myChartData="profitHistoryRaw"/>
+            </div>
+
+            <!-- 4 -->
+            <div class="card">
+                <p class="title">Leads history</p>
+                <hr>
+                <LineChart borderColor="#2BC48A" backgroundColor="#EFFAF1" :myChartData="leadsHistoryRaw"/>
+            </div>
+
+        </div>
+
+        
+
+    </div>
+</template>
+<style scoped>
+
+
+
+/* -------------------- statistics --------------- */
+
+.panel .statistics{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+}
+
+.panel .statistics .title{
+    margin: 1em;
+}
+
+.panel .statistics .card{
+    width: 500px;
+}
+
+.panel .statistics hr{
+    border: none;
+    border-bottom: 2px solid var(--color-surface-variant);
+}
+
+
+
+</style>
