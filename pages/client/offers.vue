94b417355dd8f701940ecf6dc761ca772b4f7f3b
nitesh-dev <niteshdev547@gmail.com>
2023-08-07 17:09:49 +0530

    client pages setup

--- pages/client/offers.vue
+++ pages/client/offers.vue
@@ -11,83 +11,14 @@ import {earningHistoryRaw, withdrawHistoryRaw, profitHistoryRaw, leadsHistoryRaw
             <Profile name="Nitesh kr" role="Consultant" />
         </div>
 
-        <h2>Dashboard</h2>
-        <div class="reports">
-            <div class="card">
-                <p>Total Earning</p>
-                <span>₹100,500</span>
-                <div class="progress secondary"></div>
-            </div>
-            <div class="card">
-                <p>Total Withdraw</p>
-                <span>₹100,500</span>
-                <div class="progress neutral"></div>
-            </div>
-            <div class="card">
-                <p>Total Referrals</p>
-                <span>10</span>
-            </div>
-            <div class="card">
-                <p>Total Leads Submitted</p>
-                <span>49</span>
-            </div>
-        </div>
-
-
-        <!-- statistics -->
-        <h3>Statistics</h3>
-        <div class="statistics">
-            <!-- 1 -->
-            <div class="card">
-                <p class="title">Earning history</p>
-                <hr>
-                <LineChart borderColor="#1665D8" backgroundColor="#EAEEF5" :myChartData="earningHistoryRaw"/>
-            </div>
-
-            <!-- 2 -->
-            <div class="card">
-                <p class="title">Withdraw history</p>
-                <hr>
-                <LineChart borderColor="#d33396" backgroundColor="#f5e6ef" :myChartData="withdrawHistoryRaw"/>
-            </div>
-
-            <!-- 3 -->
-            <div class="card">
-                <p class="title">Leads history</p>
-                <hr>
-                <LineChart borderColor="#2BC48A" backgroundColor="#EFFAF1" :myChartData="leadsHistoryRaw"/>
-            </div>
-
-        </div>
+        <h2>Offers & Plans</h2>
+        
 
         
 
     </div>
 </template>
 <style scoped>
-
-
-
-/* -------------------- statistics --------------- */
-
-.panel .statistics{
-    display: flex;
-    flex-wrap: wrap;
-    gap: 1rem;
-}
-
-.panel .statistics .title{
-    margin: 1em;
-}
-
-.panel .statistics .card{
-    width: 500px;
-}
-
-.panel .statistics hr{
-    border: none;
-    border-bottom: 2px solid var(--color-surface-variant);
-}
 
 
 
