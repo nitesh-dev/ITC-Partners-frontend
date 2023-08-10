866e9bbf9b6dd6e9312c7b6d5aaae0367871fc92
nitesh-dev <niteshdev547@gmail.com>
2023-08-10 14:23:06 +0530

    shimmer added in account

--- pages/client/account.vue
+++ pages/client/account.vue
@@ -4,6 +4,8 @@ import { getToken, dateTimeString } from '~/extra/utils';
 import { tabs } from '~/data/client'
 import { ConsultantAccount } from 'data/dataTypes';
 
+
+const isLoaded = ref(false)
 let token: string | null = null
 
 
@@ -21,6 +23,7 @@ async function getProfileDetail(token: string) {
     try {
         const res = await ApiConsultant.get(token)
         account.value = res
+        isLoaded.value = true
 
     } catch (error) {
         navigateTo('/')
@@ -48,94 +51,121 @@ const account = ref<ConsultantAccount>({
     <div class="panel">
         <Sidebar :active-tab="5" :tab-data="tabs"></Sidebar>
 
-        <!-- header -->
-        <div class="header">
-            <div class="card referral">
-                <span>Referral Code</span>
-                <p>{{ account.referral_code }}</p>
-            </div>
-            <Profile :image="account.profile_url" :name="account.first + ' ' + account.last" role="Admin" />
-        </div>
-
-        <h2>Account</h2>
-        <button class="primary">Edit Profile</button>
-
-        <p class="danger" v-if="!account.is_approved">Your account is not approved yet. Some of the functionality are turned off. <br>You will have all the functionality available once you will approved by the admin. You can also contact them using phone number.</p>
-        <p class="success" v-else>Your account is successfully approved by admin.</p>
-
-        <div class="profile">
-
-            <div class="equal-row">
-                <div class="card">
-                    <span>Email</span>
-                    <p>{{ account.email }}</p>
+        <template v-if="isLoaded">
+            <!-- header -->
+            <div class="header">
+                <div class="card referral">
+                    <span>Referral Code</span>
+                    <p>{{ account.referral_code }}</p>
                 </div>
-                <div class="card">
-                    <span>Name</span>
-                    <p>{{ account.first }} {{ account.last }}</p>
-                </div>
+                <Profile :image="account.profile_url" :name="account.first + ' ' + account.last" role="Admin" />
             </div>
 
-            <div class="equal-row">
-                <div class="card">
-                    <span>Phone</span>
-                    <p>{{ account.phone }}</p>
+
+            <h2>Account</h2>
+            <button class="primary">Edit Profile</button>
+
+            <p class="danger" v-if="!account.is_approved">Your account is not approved yet. Some of the functionality are
+                turned off. <br>You will have all the functionality available once you will approved by the admin. You can
+                also contact them using phone number.</p>
+            <p class="success" v-else>Your account is successfully approved by admin.</p>
+
+            <div class="profile">
+
+                <div class="equal-row">
+                    <div class="card">
+                        <span>Email</span>
+                        <p>{{ account.email }}</p>
+                    </div>
+                    <div class="card">
+                        <span>Name</span>
+                        <p>{{ account.first }} {{ account.last }}</p>
+                    </div>
                 </div>
-                <div class="card">
-                    <span>Address</span>
-                    <p>{{ account.address }}</p>
+
+                <div class="equal-row">
+                    <div class="card">
+                        <span>Phone</span>
+                        <p>{{ account.phone }}</p>
+                    </div>
+                    <div class="card">
+                        <span>Address</span>
+                        <p>{{ account.address }}</p>
+                    </div>
                 </div>
+
+                <div class="equal-row">
+                    <div class="card">
+                        <span>Pincode</span>
+                        <p>{{ account.pincode }}</p>
+                    </div>
+                    <div class="card">
+                        <span>Gender</span>
+                        <p>{{ account.gender }}</p>
+                    </div>
+                </div>
+
+                <div class="equal-row">
+                    <div class="card">
+                        <span>City</span>
+                        <p>{{ account.city }}</p>
+                    </div>
+                    <div class="card">
+                        <span>Gender</span>
+                        <p>{{ account.gender }}</p>
+                    </div>
+                </div>
+
+                <div class="equal-row">
+                    <div class="card">
+                        <span>Date of birth</span>
+                        <p>{{ dateTimeString(account.dob) }}</p>
+                    </div>
+                    <div class="card">
+                        <span>Joined at</span>
+                        <p>{{ dateTimeString(account.created_at) }}</p>
+                    </div>
+                </div>
+
+            </div>
+        </template>
+
+        <div v-else class="shimmer">
+            <box class="shine"></box>
+
+            <div>
+                <lines class="shine"></lines>
+                <lines class="shine"></lines>
+                <lines class="shine"></lines>
             </div>
 
-            <div class="equal-row">
-                <div class="card">
-                    <span>Pincode</span>
-                    <p>{{ account.pincode }}</p>
-                </div>
-                <div class="card">
-                    <span>Gender</span>
-                    <p>{{ account.gender }}</p>
-                </div>
-            </div>
+            <photo class="shine"></photo>
+            <photo class="shine"></photo>
 
-            <div class="equal-row">
-                <div class="card">
-                    <span>City</span>
-                    <p>{{ account.city }}</p>
-                </div>
-                <div class="card">
-                    <span>Gender</span>
-                    <p>{{ account.gender }}</p>
-                </div>
-            </div>
+            <br>
 
-            <div class="equal-row">
-                <div class="card">
-                    <span>Date of birth</span>
-                    <p>{{ dateTimeString(account.dob) }}</p>
-                </div>
-                <div class="card">
-                    <span>Joined at</span>
-                    <p>{{ dateTimeString(account.created_at) }}</p>
-                </div>
-            </div>
+            <box class="shine"></box>
 
+            <div>
+                <lines class="shine"></lines>
+                <lines class="shine"></lines>
+                <lines class="shine"></lines>
+            </div>
         </div>
     </div>
 
     <DialogDelete></DialogDelete>
 </template>
 <style scoped>
-
-p.danger{
+p.danger {
     color: var(--color-error);
 }
 
-p.success{
+p.success {
     color: var(--color-primary);
 }
 
-.referral{
+.referral {
     background-color: var(--color-primary);
     height: 68px;
     display: grid;
@@ -145,14 +175,14 @@ p.success{
 
 }
 
-.referral span{
+.referral span {
     color: white;
     font-size: var(--small-font);
     margin: 0;
     opacity: 0.8;
 }
 
-.referral p{
+.referral p {
     color: white;
     font-weight: bold;
     margin: 0;
